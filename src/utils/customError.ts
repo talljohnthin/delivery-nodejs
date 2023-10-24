83fd14087e0aee394a0e97de8e41224da70308c1
talljohnthin <eljustin12@gmail.com>
2023-10-24 20:09:09 +0800

    init error handler

---
+++ src/utils/customError.ts
@@ -0,0 +1,18 @@
+class CustomError extends Error {
+  public statusCode: number;
+  public status: string;
+  public isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = statusCode >= 400 && statusCode < 500 ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default CustomError;
+
+//const error = new CustomError('some error message', 404)
